4c261d79a127cea739d71795f45634168cbbd55e
Anton Lysenko <108268935+AntonLysenko553@users.noreply.github.com>
2024-02-29 14:36:59 +0300

    Remove redirection

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import * as https from "node:https";
 import * as fs from "node:fs";
-let filesArray = fs.readdirSync("public/images/asset");
+let filesArray = fs.readdirSync("/public/images/asset");
 let forTransfer = JSON.stringify(filesArray);
 function responseActions(responseObject, fileData, isIndex, isSvg) {
     responseObject.statusCode = 200;
@@ -16,12 +16,7 @@ function responseActions(responseObject, fileData, isIndex, isSvg) {
     }
 }
 const server = https.createServer(function (request, response) {
-    if(request.url === "/") {
-        response.statusCode = 301;
-        response.setHeader("Location", "/public/index.html");
-        response.end();
-    }
-    else if(request.url.startsWith("/public")) {
+    if(request.url.startsWith("/public")) {
         if(request.url.endsWith("index.html")) {
             fs.readFile(request.url, function (error, data) {
                 responseActions(response, data, true, false);
